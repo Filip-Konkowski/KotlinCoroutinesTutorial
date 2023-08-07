3ed56d6c51af270842ad4bd9bef26c85074b2a84
filipkonkowski <filip.konkowski@icabbi.com>
2023-08-07 17:29:56 +0100

    Try coroutine functions

---
+++ initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine3.kt
@@ -0,0 +1,45 @@
+import kotlinx.coroutines.*
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.coroutines.CoroutineContext
+
+
+fun main() = runBlocking {
+
+
+    logMessage3("Hello")
+
+    val jobs: List<Job> = (1..5).map {
+        launch(start = CoroutineStart.LAZY) {
+            logMessage3("Started Coroutine $it")
+            delay(100)
+            logMessage3("Ended Coroutine $it")
+        }
+    }
+    logMessage3("Created all Coroutines")
+    jobs.forEach { it.start() }
+    logMessage3("Finished all Coroutines")
+
+    logMessage3("world")
+}
+
+
+fun logMessage3(msg: String) {
+    println("Running on: [${Thread.currentThread().name}] | $msg")
+}
+
+
+fun CoroutineScope.logContext3(id: String) {
+    coroutineContext.logDetails3(id)
+}
+
+
+fun CoroutineContext.logDetails3(id: String) {
+    sequenceOf(
+        Job,
+        ContinuationInterceptor,
+        CoroutineExceptionHandler,
+        CoroutineName
+    )
+        .mapNotNull { key -> this[key] }
+        .forEach { logMessage3("id: $id ${it.key} = ${it}") }
+}
