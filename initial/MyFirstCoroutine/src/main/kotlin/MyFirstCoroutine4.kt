16164d864ed32752c324010c67b6edbe050558d2
filipkonkowski <filip.konkowski@icabbi.com>
2023-08-17 13:52:31 +0100

    Try coroutine add yield()

--- initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine4.kt
+++ initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine4.kt
@@ -69,37 +69,37 @@ import kotlinx.coroutines.*
 
 
 //TODO - 3 We have successfully used the isActive property, but there is another way, the ensureActive() function, lets modify the code to use it.
-fun main() = runBlocking {
+//fun main() = runBlocking {
+//
+//
+//    val scope = CoroutineScope(Dispatchers.Default + CoroutineName("ensureActive"))
+//
+//    val startTime = System.currentTimeMillis()
+//    val job1 = scope.launch() {
+//        var nextPrintTime = startTime
+//        var i = 0
+//        while (i < 5) {
+//            // Hint: This may be a good place to ensure our coroutine is still active.
+//            // print a message twice a second
+//            ensureActive()
+//            if (System.currentTimeMillis() >= nextPrintTime) {
+//                logMessage4("Hello World ${i++}")
+//                nextPrintTime += 500L
+//            }
+//        }
+//
+//    }
+//
+//    delay(1000L)
+//    logMessage4("Cancel!")
+//    job1.cancel()
+//    logMessage4("Done!")
+//    delay(5000)
+//
+//}
 
 
-    val scope = CoroutineScope(Dispatchers.Default + CoroutineName("ensureActive"))
 
-    val startTime = System.currentTimeMillis()
-    val job1 = scope.launch() {
-        var nextPrintTime = startTime
-        var i = 0
-        while (i < 5) {
-            // Hint: This may be a good place to ensure our coroutine is still active.
-            // print a message twice a second
-            ensureActive()
-            if (System.currentTimeMillis() >= nextPrintTime) {
-                logMessage4("Hello World ${i++}")
-                nextPrintTime += 500L
-            }
-        }
-
-    }
-
-    delay(1000L)
-    logMessage4("Cancel!")
-    job1.cancel()
-    logMessage4("Done!")
-    delay(5000)
-
-}
-
-
-/*
 //TODO - 4 All Kotlin Coroutine framework 'suspend' functions are cooperative, so by using them our code also becomes cooperative, lets use the yield function.
 fun main() = runBlocking {
 
@@ -112,6 +112,7 @@ fun main() = runBlocking {
         while (i < 5) {
             //Hint: this maybe a good place to yeild.
             // print a message twice a second
+            yield()
             if (System.currentTimeMillis() >= nextPrintTime) {
                 logMessage4("Hello World ${i++}")
                 nextPrintTime += 500L
@@ -128,7 +129,7 @@ fun main() = runBlocking {
 
 }
 
-*/
+
 
 /*
 //TODO - 4 All Kotlin Coroutine framework 'suspend' functions are cooperative, so by using them our code also becomes cooperative, lets use the delay function.
