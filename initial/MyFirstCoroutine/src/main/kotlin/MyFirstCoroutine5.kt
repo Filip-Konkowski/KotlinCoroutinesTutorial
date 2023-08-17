d41c2502f74e170dfbee1161b2f983a0768d0fbf
filipkonkowski <filip.konkowski@icabbi.com>
2023-08-17 15:19:40 +0100

    Explain supervisor job exception with runCatching

--- initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine5.kt
+++ initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine5.kt
@@ -48,21 +48,44 @@ import kotlin.coroutines.CoroutineContext
 //    logMessage5("The End")
 //}
 
+//
+//fun main() = runBlocking {
+//
+//    val scope = CoroutineScope(Job())
+//    val sharedJob = SupervisorJob()
+//
+//    scope.launch(sharedJob) {
+//        delay(10)
+//        logMessage5("Hello")
+//    }
+//
+//    scope.launch(sharedJob) {
+//        throw IndexOutOfBoundsException("oops")
+//        delay(10)
+//        logMessage5("World")
+//    }
+//
+//    delay(100)
+//    logMessage5("The End")
+//}
+
 
 fun main() = runBlocking {
 
     val scope = CoroutineScope(Job())
-    val sharedJob = SupervisorJob()
 
-    scope.launch(sharedJob) {
-        delay(10)
-        logMessage5("Hello")
-    }
+    scope.launch {
+        val result = runCatching {
+            throw IndexOutOfBoundsException("oops")
+        }
 
-    scope.launch(sharedJob) {
-        throw IndexOutOfBoundsException("oops")
-        delay(10)
-        logMessage5("World")
+        if (result.isSuccess) {
+            logMessage5("I am Happy!")
+        }
+
+        if (result.isFailure) {
+            logMessage5("I am Sad :(")
+        }
     }
 
     delay(100)
