ab3b2112744bd937ab66422e5c4abad4b758cf66
filipkonkowski <filip.konkowski@icabbi.com>
2023-08-17 14:32:46 +0100

    Explain supervisor job cancel part 2

--- initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine5.kt
+++ initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine5.kt
@@ -2,20 +2,47 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 
+//fun main() = runBlocking {
+//
+//    val scope = CoroutineScope(SupervisorJob())
+//
+//    scope.launch {
+//        delay(10)
+//        logMessage5("Hello")
+//    }
+//
+//    scope.launch {
+//        throw IndexOutOfBoundsException("oops")
+//        delay(10)
+//        logMessage5("World")
+//    }
+//
+//    delay(100)
+//    logMessage5("The End")
+//}
+
+
 fun main() = runBlocking {
 
-    val scope = CoroutineScope(SupervisorJob())
+    val scope = CoroutineScope(Job())
 
     scope.launch {
-        delay(10)
-        logMessage5("Hello")
+        // this ensure that coroutines are in the supervisor scope
+        supervisorScope {
+            launch {
+                delay(10)
+                logMessage5("Hello")
+            }
+
+            launch {
+                throw IndexOutOfBoundsException("oops")
+                delay(10)
+                logMessage5("World")
+            }
+        }
     }
 
-    scope.launch {
-        throw IndexOutOfBoundsException("oops")
-        delay(10)
-        logMessage5("World")
-    }
+
 
     delay(100)
     logMessage5("The End")
