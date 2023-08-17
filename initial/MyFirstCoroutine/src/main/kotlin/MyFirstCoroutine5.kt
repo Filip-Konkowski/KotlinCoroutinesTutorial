91268f3d6b54361479c1daaf71047a2c7fab2069
filipkonkowski <filip.konkowski@icabbi.com>
2023-08-17 14:38:02 +0100

    Explain supervisor job exception with shared job

--- initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine5.kt
+++ initial/MyFirstCoroutine/src/main/kotlin/MyFirstCoroutine5.kt
@@ -22,27 +22,48 @@ import kotlin.coroutines.CoroutineContext
 //}
 
 
+//fun main() = runBlocking {
+//
+//    val scope = CoroutineScope(Job())
+//
+//    scope.launch {
+//        // this ensure that coroutines are in the supervisor scope
+//        supervisorScope {
+//            launch {
+//                delay(10)
+//                logMessage5("Hello")
+//            }
+//
+//            launch {
+//                throw IndexOutOfBoundsException("oops")
+//                delay(10)
+//                logMessage5("World")
+//            }
+//        }
+//    }
+//
+//
+//
+//    delay(100)
+//    logMessage5("The End")
+//}
+
+
 fun main() = runBlocking {
 
     val scope = CoroutineScope(Job())
+    val sharedJob = SupervisorJob()
 
-    scope.launch {
-        // this ensure that coroutines are in the supervisor scope
-        supervisorScope {
-            launch {
-                delay(10)
-                logMessage5("Hello")
-            }
-
-            launch {
-                throw IndexOutOfBoundsException("oops")
-                delay(10)
-                logMessage5("World")
-            }
-        }
+    scope.launch(sharedJob) {
+        delay(10)
+        logMessage5("Hello")
     }
 
-
+    scope.launch(sharedJob) {
+        throw IndexOutOfBoundsException("oops")
+        delay(10)
+        logMessage5("World")
+    }
 
     delay(100)
     logMessage5("The End")
